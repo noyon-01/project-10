28a9ddec80ca34da1a4342415185bb3a2f68ca77
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-04-28 09:18:06 +0600

    font-optimation and daisy ui and setup is added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["400", "500", "700"],
 });
 
 export const metadata = {
@@ -18,11 +14,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+    <html lang="en" data-theme="light" className="h-full antialiased">
+      <body className={`${poppins.className} min-h-full flex flex-col`}>
+        <main className="container mx-auto">{children}</main>
+      </body>
     </html>
   );
 }
